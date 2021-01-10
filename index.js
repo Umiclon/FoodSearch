8661eeaff403173da5a487ea1a76549a1125bf70
roger <rogerhuang257@gmail.com>
2021-01-10 02:19:41 -0800

    set up yelp fusion api

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
+//https://blog.bearer.sh/yelp-api-how-to/
 const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('3000 in use'));
 app.use(express.static('webpage'));
 
-// function setup() {
-//     createCanvas(200,200);
-// }
+const axios = require("axios");
+let API_KEY = config.SECRET_API_KEY;
 
-// function draw(){
-//     background(0);
-//     fill(100,100,100);
-//     rect(100,100,100,100);
-// }
+let yelpREST = axios.create({
+    baseURL: "https://api.yelp.com/v3/",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-type": "application/json",
+    },
+  })
+  
+  yelpREST("/businesses/search", {
+    params: {
+      location: "vancouver",
+      term: "restaurant",
+      limit: 10,
+    },
+  }).then(({ data }) => {
+    let { businesses } = data
+    businesses.forEach((b) => {
+      console.log("Name: ", b.name)
+    })
+  })
 
+  
